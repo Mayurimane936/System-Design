95c75f93e3b7781494b8def6d65103ff4453cebc
Mayurimane936 <mayurimane936@gmail.com>
2026-09-26 14:21:20 +0530

    factory design pattern

---
+++ design_patterns/factory_pattern/payment_service.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include<iostream>
+
+using namespace std;
+
+enum class PaymentTypes {
+    UPI,
+    Card,
+    NetBanking
+};
+
+class Payment{
+public:
+   virtual void pay() = 0;
+   virtual ~ Payment(){}
+};
+
+class UPI: public Payment {
+public:
+    void pay() {
+        cout << "Paid using UPI";
+    }
+};
+
+class Card: public Payment {
+public:
+    void pay() {
+        cout << "Paid using Card";
+    }
+};
+
+class NetBanking: public Payment {
+public:
+    void pay() {
+        cout << "Paid using Net Banking";
+    }
+};
+
+class PaymentFactory {
+    public: 
+        Payment *createPayment(PaymentTypes method){
+            if(method == PaymentTypes:: UPI){
+                return new UPI();
+            }else if(method == PaymentTypes:: Card){
+                return new Card();
+            }else {
+                return new NetBanking();
+            }
+        }
+};
+
+
+int main(){
+    PaymentFactory doPayment;
+    Payment *p = doPayment.createPayment(PaymentTypes::NetBanking);
+    p->pay();
+    delete p;
+    return 0;
+}
